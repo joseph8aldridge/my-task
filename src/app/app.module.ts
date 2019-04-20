f053540258c7962726b89278258510d384bbf410
Joseph Aldridge <joseph8aldridge1995@gmail.com>
2019-04-20 15:39:02 +0300

    base skeleton, app.component as container, try it with home.component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ViewComponent } from './view/view.component';
+import { WishlistComponent } from './wishlist/wishlist.component';
+import { ContactsComponent } from './contacts/contacts.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+
+const myTaskRoutes: Routes = [
+    {path: '', component: HomeComponent},    
+    {path: 'about', component: AboutComponent},    
+    {path: 'view', component: ViewComponent},    
+    {path: 'wishlist', component: WishlistComponent},    
+    {path: 'contacts', component: ContactsComponent},    
+    {path: '**', component: NotFoundComponent}    
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    ViewComponent,
+    WishlistComponent,
+    ContactsComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    RouterModule.forRoot(myTaskRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
